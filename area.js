bfd6a915fd7513f5f60ad9bf51020ce51f5f4a5b
saurabhchirde <saurabh123.chirde@gmail.com>
2021-11-09 12:29:29 +0530

    update: hypotenuse

---
+++ area.js
@@ -0,0 +1,23 @@
+const base = document.querySelector("#base");
+const height = document.querySelector("#height");
+const btnCalculate = document.querySelector(".calculate");
+const btnClear = document.querySelector(".clear");
+const output = document.querySelector(".output");
+
+function area() {
+  const b = base.value;
+  const h = height.value;
+  if (b === "" || h === "" || b === "0" || h === "0") {
+    output.innerText = "Base or height cannot be blank or zero";
+  } else {
+    output.innerText = "Area of a triangle is : " + (1 / 2) * b * h;
+  }
+}
+
+btnCalculate.addEventListener("click", area);
+
+btnClear.addEventListener("click", function () {
+  base.value = "";
+  height.value = "";
+  output.innerText = "";
+});
---
+++ hypoScript.js
@@ -0,0 +1,24 @@
+const length1 = document.querySelector("#length1");
+const length2 = document.querySelector("#length2");
+const btnCalculate = document.querySelector(".calculate");
+const btnClear = document.querySelector(".clear");
+const output = document.querySelector(".output");
+
+function hypotenuse() {
+  const l1 = length1.value;
+  const l2 = length2.value;
+  if (l1 === "" || l1 === 0 || l2 === "" || l2 === 0) {
+    output.innerText = "Enter valid lengths";
+  } else {
+    output.innerText =
+      "Hypotenuse of a triangle is : " + Math.sqrt(l1 * l1 + l2 * l2);
+  }
+}
+
+btnCalculate.addEventListener("click", hypotenuse);
+
+btnClear.addEventListener("click", () => {
+  length1.value = "";
+  length2.value = "";
+  output.innerText = "";
+});
