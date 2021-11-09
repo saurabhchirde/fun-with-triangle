e86bcc19cfc6c54b5ee859c9bdef9bbbd99019d7
saurabhchirde <saurabh123.chirde@gmail.com>
2021-11-09 13:33:18 +0530

    isTriangle: used querySelectorAll

--- isTriangleScript.js
+++ isTriangleScript.js
@@ -1,17 +1,14 @@
-const input1 = document.querySelector("#input1");
-const input2 = document.querySelector("#input2");
-const input3 = document.querySelector("#input3");
+const inputAngles = document.querySelectorAll(".inputAngle");
 const output = document.querySelector(".output");
 const btnCheck = document.querySelector(".isTriangle");
 const btnClear = document.querySelector(".clear");
 
 function calculateSumOfAngles() {
   let sum = 0;
-  const a1 = Number(input1.value);
-  const a2 = Number(input2.value);
-  const a3 = Number(input3.value);
+  const a1 = Number(inputAngles[0].value);
+  const a2 = Number(inputAngles[1].value);
+  const a3 = Number(inputAngles[2].value);
   sum = a1 + a2 + a3;
-  console.log("Sum :" + sum);
   if (sum === 180) {
     output.innerText = "Angles form a triangle";
   } else {
@@ -19,14 +16,13 @@ function calculateSumOfAngles() {
       "These angles cannot form a triangle, try with other angles";
   }
 }
-//check-Btn
+
 btnCheck.addEventListener("click", calculateSumOfAngles);
 
-//clear-Btn
 btnClear.addEventListener("click", function () {
   sum = 0;
-  input1.value = "";
-  input2.value = "";
-  input3.value = "";
+  inputAngles[0].value = "";
+  inputAngles[1].value = "";
+  inputAngles[2].value = "";
   output.innerText = "";
 });
